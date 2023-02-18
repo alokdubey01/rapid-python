58600ea386784a4a554b4158c2b362bffe5fed5d
Alok Kumar Dubey <72490595+alokdubey01@users.noreply.github.com>
2023-02-18 22:08:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route('/')
+def default():
+    return "Hello! I am a home page"
+
 @app.route('/quote', methods=['GET', 'POST'])
 def home():
     from random_word import RandomWords
